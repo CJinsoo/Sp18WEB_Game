7ff736dd55619d7359066b67b537496794ca0a9d
Jinsoo Choi <highgenius94@gmail.com>
2018-03-12 16:11:07 -0400

    3/12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import  {RouterModule, Routes}  from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MessagesComponent,
+    HomeComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([//array of router object
+      {path: 'home', component: HomeComponent},
+      {path: 'game', component: GameComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'}
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]//put AppComponent right inside index.html.
 })
-export class AppModule { }
+export class AppModule { }//put nothing in the module
